3a9f87966e20d0ec5059f9951473c3429865f9e6
Kudrman <kudrman_93@mail.ru>
2024-01-11 16:38:53 +0700

    - добавление простой логики перемещения противника - добавление анимирования противника

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour
+{
+    private const string MovementHorizontalKey = "Horizontal";
+    private const string MovementVerticalKey = "Vertical";
+
+    private Animator _animator;
+    private NavMeshAgent _navMeshAgent;
+
+    private Transform _playerTransform;
+
+    private Vector3 _prevPosition;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        _animator = GetComponentInChildren<Animator>();
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+
+        _playerTransform = FindAnyObjectByType<Player>().transform;
+
+        _prevPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        SetTargetPosition(_playerTransform.position);
+        RefreshAnimation();
+    }
+
+    private void SetTargetPosition(Vector3 position)
+    {
+        _navMeshAgent.SetDestination(position);
+    }
+
+    private void RefreshAnimation()
+    {
+        Vector3 curPosition = transform.position;
+        Vector3 deltaMove = curPosition - _prevPosition;
+        _prevPosition = curPosition;
+
+        deltaMove.Normalize();
+
+        float relatedX = Vector3.Dot(deltaMove, transform.right);
+        float relatedY = Vector3.Dot(deltaMove, transform.forward);
+
+        _animator.SetFloat(MovementHorizontalKey, relatedX);
+        _animator.SetFloat(MovementVerticalKey, relatedY);
+    }
+}
