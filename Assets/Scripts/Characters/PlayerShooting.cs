ff49f6575dfec614e917eb37dbbc88ecf7e49517
Kudrman <kudrman_93@mail.ru>
2024-02-08 23:22:40 +0700

    - добавление автоперезарядки, при окончании обоймы

--- Assets/Scripts/Characters/PlayerShooting.cs
+++ Assets/Scripts/Characters/PlayerShooting.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class PlayerShooting : CharacterShooting
 {
 
+    [SerializeField] protected bool _autoReloading = true;
+
     protected override void OnInit()
     {
         base.OnInit();
@@ -13,12 +15,26 @@ public class PlayerShooting : CharacterShooting
         if (Input.GetMouseButton(0))
         {
             Shoot();
+            AutoReloading();
         }
     }
 
     protected override void Reloading()
     {
         if ((!CheckHasBulletsInRow() && Input.GetMouseButton(0)) || Input.GetKeyDown(KeyCode.R))
+        {
+            Reload();
+        }
+    }
+
+    private void AutoReloading()
+    {
+        if (!_autoReloading)
+        {
+            return;
+        }
+
+        if (!CheckHasBulletsInRow())
         {
             Reload();
         }
